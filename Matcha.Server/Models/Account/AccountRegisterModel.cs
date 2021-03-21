d1a81b101408a16f373661c13b125a0bc008f4f0
dbendu <dbendu@yandex.ru>
2021-03-21 16:24:04 +0300

    Добавлена модель регистрации пользователя

---
+++ Matcha.Server/Models/Account/AccountRegisterModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Matcha.Server.Models.Account
+{
+    [BindProperties(SupportsGet = true)]
+    public sealed class AccountRegisterModel
+    {
+        [JsonProperty(PropertyName = "Email")]
+        public string Email { get; set; }
+
+        [JsonProperty(PropertyName = "Login")]
+        public string Login { get; set; }
+
+        [JsonProperty(PropertyName = "Password")]
+        public string Password { get; set; }
+
+        [JsonProperty(PropertyName = "Name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "Surname")]
+        public string Surname { get; set; }
+    }
+}
