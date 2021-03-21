6ff3870b6515155a89a55533c5f2cd34bdf16c75
dbendu <dbendu@yandex.ru>
2021-03-21 16:09:43 +0300

    Добавлена модель ответа сервера

---
+++ Matcha.Core/Models/Response/ResponseModel.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net;
+
+namespace server.Response
+{
+    public sealed partial class ResponseModel
+    {
+        [JsonProperty(PropertyName = "Status")]
+        public readonly ResponseStatus Status;
+
+        [JsonProperty(PropertyName = "Content")]
+        public Dictionary<string, object> Content { get; set; }
+
+        public ResponseModel(ResponseStatus status, Dictionary<string, object> content)
+        {
+            Status = status;
+            Content = content;
+        }
+
+        public ResponseModel(HttpStatusCode code, string errorMessage)
+        {
+            Status = new ResponseStatus(code, errorMessage);
+            Content = null;
+        }
+
+        public static ResponseModel Ok() => new ResponseModel(HttpStatusCode.OK, null);
+    }
+}
