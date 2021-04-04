6d1feba56af62fed46f9511bde532043570e1b8e
dbendu <dbendu@yandex.ru>
2021-04-05 01:01:21 +0300

    Реализовано несколько видов сортировок аккаунтов пользователей

---
+++ Matcha.Server/Models/Users/SortParametersModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Matcha.Server.Models.Users
+{
+    public sealed record SortParametersModel : IValidatableObject
+    {
+        [Required]
+        public int Page { get; set; }
+
+        [Required]
+        public int Size { get; set; }
+
+        public string OrderBy { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (OrderBy is null)
+                OrderBy = "id";
+
+            if (KnownOrderMethods.Contains(OrderBy) is false)
+                yield return new ValidationResult($"Неизвестный вид сортировки: {OrderBy}. Поддерживаемые виды: {string.Join(',', KnownOrderMethods)}");
+        }
+
+        public static HashSet<string> KnownOrderMethods = new HashSet<string>
+        {
+            "id",
+            "age",
+            //"location",
+            "rating",
+            //"common_tags"
+        };
+    }
+}
