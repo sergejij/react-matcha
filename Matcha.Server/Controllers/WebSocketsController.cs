ce3673271156695de946e150e85cc2366d55db78
dbendu <dbendu@yandex.ru>
2021-04-18 13:04:51 +0300

    Сохранение

--- Matcha.Server/Controllers/WebSocketsController.cs
+++ Matcha.Server/Controllers/WebSocketsController.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using server.Response;
 using System;
 using System.Net;
+using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -27,16 +28,20 @@ namespace Matcha.Server.Controllers
             var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
 
             var buffer = new byte[1024 * 4];
-            var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
 
-            while (!result.CloseStatus.HasValue)
+            WebSocketReceiveResult result = null;
+            do
             {
                 var serverMsg = Encoding.UTF8.GetBytes($"Полученное сообщение: {Encoding.UTF8.GetString(buffer)}");
                 Console.WriteLine($"MESSAGE:\t\t\t{Encoding.UTF8.GetString(serverMsg)}");
                 await webSocket.SendAsync(new ArraySegment<byte>(serverMsg, 0, serverMsg.Length), result.MessageType, result.EndOfMessage, CancellationToken.None);
 
                 result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "ДАТВИДАНИЯ", CancellationToken.None);
             }
+            while (result.CloseStatus.HasValue is false);
+
             await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
 
             return ResponseModel.OK.ToResult();
