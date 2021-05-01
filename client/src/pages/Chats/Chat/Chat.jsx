41dade270c2f37f6143f53cce377b708f1f4edc5
sergejij <ushakoff.sergej2010@yandex.ru>
2021-05-01 16:58:35 +0300

    is almost ready:)"

--- client/src/pages/Chats/Chat/Chat.jsx
+++ client/src/pages/Chats/Chat/Chat.jsx
@@ -5,12 +5,16 @@ import { ChatStyled } from './styled';
 import ChatMessages from './ChatMessages/ChatMessages';
 import ChatInput from './ChatInput/ChatInput';
 
-const Chat = ({ userId, socket, newMessage }) => (
-  <ChatStyled>
-    <ChatHeader userId={userId} />
-    <ChatMessages newMessage={newMessage} userId={userId} />
-    <ChatInput socket={socket} userId={userId} />
-  </ChatStyled>
-);
+const Chat = ({ userId, socket, newMessage }) => {
+    const [messages, setMessages] = React.useState([]);
+
+    return (
+        <ChatStyled>
+            <ChatHeader userId={userId} />
+            <ChatMessages messages={messages} setMessages={setMessages} newMessage={newMessage} userId={userId} />
+            <ChatInput setMessages={setMessages} socket={socket} userId={userId} />
+        </ChatStyled>
+    );
+}
 
 export default Chat;
--- client/src/pages/Chats/Chat/ChatInput/ChatInput.jsx
+++ client/src/pages/Chats/Chat/ChatInput/ChatInput.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import SendIcon from '@material-ui/icons/Send';
 import { ChatInputStyled, TA, SendIconStyled } from './styled';
-const ChatInput = ({userId, socket}) => {
+const ChatInput = ({userId, socket, setMessages}) => {
   const [message, setMessage] = React.useState('');
 
   const sendMessage = () => {
       console.log(socket)
     if (socket) {
-        console.log("userId, message", userId, message);
         socket.sendMessage(message, userId);
+        setMessages(messages => [{MyMessage: true, Content: message}].concat(messages));
     }
     setMessage('');
   }
